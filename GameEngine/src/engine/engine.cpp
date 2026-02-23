2dbf98e010bd683e6b75404d94e0874e63a9e2a7
Taha Ozer <taha.ozer@student.uhasselt.be>
2026-02-23 20:21:22 +0100

    modular

--- GameEngine/src/engine/engine.cpp
+++ GameEngine/src/engine/engine.cpp
@@ -1,10 +1,28 @@
 #include "engine.hpp"
 
-int main()
+Engine::Engine(int width, int height, std::string title, int fps, int fs) : m_width{width}, m_height{height}, m_title{title}, m_fps{fps}, m_fs{fs} {};
+
+void Engine::run() const
 {
-    Engine test{800, 600, "testing", 60, 1};
-
-    test.run();
-
-    return 0;
+    if (m_fs)
+    {
+        sf::RenderWindow window(sf::VideoMode(), m_title, sf::Style::Fullscreen);
+    }
+    else
+    {
+        sf::RenderWindow window(sf::VideoMode(m_width, m_height), m_title);
+    }
+    window.s while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+            {
+                window.close();
+            }
+        }
+        window.clear(sf::Color::Black);
+        window.display();
+    }
 }
--- GameEngine/src/engine/engine.hpp
+++ GameEngine/src/engine/engine.hpp
@@ -6,30 +6,7 @@ class Engine
 {
 public:
     Engine(int width, int height, std::string title, int fps, int fs) : m_width{width}, m_height{height}, m_title{title}, m_fps{fps}, m_fs{fs} {};
-    void run() const
-    {
-        sf::RenderWindow window(sf::VideoMode(m_width, m_height), m_title);
-        window.setFramerateLimit(m_fps);
-        if (m_fs == 1)
-        {
-            sf::Style::Fullscreen;
-        }
-
-        while (window.isOpen())
-        {
-            sf::Event event;
-            while (window.pollEvent(event))
-            {
-                if (event.type == sf::Event::Closed)
-                {
-
-                    window.close();
-                }
-            }
-            window.clear(sf::Color::Black);
-            window.display();
-        }
-    }
+    void run() const;
 
 private:
     int m_width{};
